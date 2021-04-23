246c20ba7ade22d5aa1da9dbd10994cb74d1d279
Renan Plathon <rplathon@gmail.com>
2021-04-23 01:02:51 -0300

    init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (_: Request, response: Response) => {
+    response.status(200).json({result: 'ok!'})
+})
+
+app.listen(3000)
